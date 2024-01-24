ee6d1fe410496362ffe981d54db4ab82025fe585
Nigel Lobo <nigellobo675@gmail.com>
2024-01-23 20:46:04 -0500

    cleanup

--- src/main/java/com/ecommerce/ecommerce/Checkout/CheckoutService.java
+++ src/main/java/com/ecommerce/ecommerce/Checkout/CheckoutService.java
@@ -1,5 +1,6 @@
-// package com.ecommerce.ecommerce.Checkout;
+package com.ecommerce.ecommerce.Checkout;
 
-// public class CheckoutService {
-
-// }
+public class CheckoutService {
+    // service should contain business logic for completing a transaction of a user
+    // cart
+}
